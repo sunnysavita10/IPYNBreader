fd22855ca47032fa7b29d3e0bdebcb15ab407b4a
SUNNY SAVITA <engsunny3365@gmail.com>
2023-03-31 19:23:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md","r",encoding="utf-8") as f:
     long_discription=f.read()
 
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 
 REPO_NAME = "demo_project"
 AUTHOR_USER_NAME = "sunny savita"
