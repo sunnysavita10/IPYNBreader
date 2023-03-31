987145dcc6f35f357642ba56e9206f0084b8b1e8
sunnysavita10 <engsunny3365@gmail.com>
2023-03-31 16:01:13 +0530

    main code updated

---
+++ src/demo_project/site.py
@@ -0,0 +1,28 @@
+from IPython import display
+from ensure import ensure_annotations
+import urllib 
+from demo_project.custom_exception import InvalidURLException
+from demo_project.logger import logger
+
+@ensure_annotations
+def is_valid(URL:str)->bool:
+    try:
+        response_status=urllib.request.urlopen(URL).getcode()
+        assert response_status
+        logger.debug(f"response_status:{response_status}")
+        return True    
+    except Exception as e:
+        logger.exception(e)
+        return False
+
+@ensure_annotations
+def render_site(URL: str, width: str = "100%", height: str = "600") -> str:
+    try:
+        if is_valid(URL):
+            response = display.IFrame(src=URL, width=width, height=height)
+            display.display(response)
+            return "success"
+        else:
+            raise InvalidURLException
+    except Exception as e:
+        raise e
